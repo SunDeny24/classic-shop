32b374be77a04f353791b7b2ca0cb44792129f17
SunDeny <genome5252@gmail.com>
2026-05-06 16:55:26 +0900

    feat : 이미지 최적화 위해 일반 img태그를 next/image로 교체 및 최적화 적용

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    /* config options here */
-    devIndicators: {
-        buildActivity: true,
-    },
+  /* config options here */
+  devIndicators: {
+    buildActivity: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "shopping-phinf.pstatic.net", //네이버 쇼핑
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "i.ytimg.com", //유튜브 섬네일
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
--- src/components/ui/ProductCard.jsx
+++ src/components/ui/ProductCard.jsx
@@ -1,43 +1,63 @@
 //상품 카드 컴포넌트
 // src/components/ui/ProductCard.jsx
-'use client';
+"use client";
 
-import { useState } from 'react';
-import Link from 'next/link';
-import LikeButton from './LikeButton';
+import { useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import LikeButton from "./LikeButton";
 
 export default function ProductCard({ productData }) {
-    //이미지 로딩위한 상태관리
-    const [isImageLoaded, setIsImageLoaded] = useState(false);
+  //이미지 로딩위한 상태관리
+  const [isImageLoaded, setIsImageLoaded] = useState(false);
 
-    return (
-        <div className="overflow-hidden group">
-            <Link
-                href={{
-                    pathname: `/product/${productData.productId}`,
-                    query: {
-                        data: encodeURIComponent(JSON.stringify(productData)),
-                    },
-                }}
-            >
-                <div className="relative aspect-[3/4] overflow-hidden bg-zinc-50">
-                    {!isImageLoaded && <div className="absolute inset-0 z-10 animate-pulse bg-zinc-200" />}
-                    <img
-                        src={productData.image}
-                        alt={productData.title}
-                        className={`w-full h-full object-cover transition-all duration-500 ${
-                            isImageLoaded ? 'opacity-100 scale-100' : 'opacity-0 scale-105'
-                        } group-hover:scale-105`}
-                        onLoad={() => setIsImageLoaded(true)}
-                    />
-                    <LikeButton productData={productData} />
-                </div>
-                <div className="py-5 px-1">
-                    <p className="text-[12px]  sm:text-[12px] font-semibold text-gray-600 mb-1">{productData.brand}</p>
-                    <h3 className="text-[12px] sm:text-[14px] font-normal text-gray-900 ">{productData.title}</h3>
-                    <p className="text-lg sm:text-xl font-semibold text-gray-950 mt-1">{productData.lprice}</p>
-                </div>
-            </Link>
+  return (
+    <div className="overflow-hidden group">
+      <Link
+        href={{
+          pathname: `/product/${productData.productId}`,
+          query: {
+            data: encodeURIComponent(JSON.stringify(productData)),
+          },
+        }}
+      >
+        <div className="relative aspect-[3/4] overflow-hidden bg-zinc-50">
+          {!isImageLoaded && (
+            <div className="absolute inset-0 z-10 animate-pulse bg-zinc-200" />
+          )}
+          <Image
+            src={productData.image}
+            alt={productData.title}
+            fill
+            sizes="300px" //sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw,33vw"
+            className={`object-cover transition-all duration-500 ${
+              isImageLoaded ? "opacity-100 scale-100" : "opacity-0 scale-105"
+            } group-hover:scale-105`}
+            onLoad={() => setIsImageLoaded(true)}
+            quality={60}
+          />
+          {/*<img
+            src={productData.image}
+            alt={productData.title}
+            className={`w-full h-full object-cover transition-all duration-500 ${
+              isImageLoaded ? "opacity-100 scale-100" : "opacity-0 scale-105"
+            } group-hover:scale-105`}
+            onLoad={() => setIsImageLoaded(true)}
+          />*/}
+          <LikeButton productData={productData} />
         </div>
-    );
+        <div className="py-5 px-1">
+          <p className="text-[12px]  sm:text-[12px] font-semibold text-gray-600 mb-1">
+            {productData.brand}
+          </p>
+          <h3 className="text-[12px] sm:text-[14px] font-normal text-gray-900 ">
+            {productData.title}
+          </h3>
+          <p className="text-lg sm:text-xl font-semibold text-gray-950 mt-1">
+            {productData.lprice}
+          </p>
+        </div>
+      </Link>
+    </div>
+  );
 }
