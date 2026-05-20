731c8f424d7c6980695b30bd956116e95a28eb7b
SunDeny <genome5252@gmail.com>
2026-05-20 11:43:11 +0900

    refactor : 공통 레이아웃 컴포넌트 TS 전환

---
+++ src/components/layout/Logo.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export default function Logo() {
+  return (
+    <div className="shrink-0">
+      <Link href="/">
+        <Image
+          src="/images/Skipick.svg"
+          alt="Skipick"
+          width={130}
+          height={50}
+          priority
+        />
+      </Link>
+    </div>
+  );
+}
